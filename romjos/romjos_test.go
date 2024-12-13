54867361aae1727f94fc40e09f43bd7b2a234837
Horacio Cortez <leohermoso18@gmail.com>
2024-12-12 22:35:56 -0300

    romjos

---
+++ romjos/romjos_test.go
@@ -0,0 +1,17 @@
+package romjos
+
+import "fmt"
+
+func test() {
+
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+	fmt.Println("asefpksaofp")
+
+}
